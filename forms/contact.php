27e4cde66c5472a7bfdfd95127405b175a28e2f4
niassy1903 <niassy.lamine10@gmail.com>
2025-10-02 17:44:48 +0000

    mise a jour

--- forms/contact.php
+++ forms/contact.php
@@ -2,7 +2,10 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../vendor/autoload.php'; // adapte le chemin si besoin
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
 
 header('Content-Type: text/plain');
 
@@ -14,13 +17,13 @@ try {
     $mail->Host = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
     $mail->Username = 'niassy.lamine10@gmail.com';         // Ton email Gmail
-    $mail->Password = 'ymgrbncasjhskxxr';                  // Mot de passe d'application
+    $mail->Password = 'efhi acpc neyg pziv';                  // Mot de passe d'application
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port = 587;
 
     // Expéditeur et destinataire
     $mail->setFrom($_POST['email'], $_POST['name']);
-    $mail->addAddress('niassylamine10@gmail.com'); // Ton email de réception
+    $mail->addAddress('niassy.lamine10@gmail.com'); // Ton email de réception
 
     // Contenu du mail
     $mail->isHTML(false);
